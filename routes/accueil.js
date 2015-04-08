ee70775a932391cc03044741c6a1c70b5a2b30b5
Nathan Urbain <nathan.urbain@etu.unistra.fr>
2015-04-08 14:15:27 +0200

    Ajout 1er gestion des tweets

--- routes/accueil.js
+++ routes/accueil.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+var tweets = [];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('accueil', { title: 'Hauer' });
+  res.render('accueil', { title: 'Hauer', tweets: tweets });
+});
+
+router.post('/', function(req, res, next) {
+    tweets.unshift(req.body.tweet);
+    res.redirect('/accueil')
 });
 
 module.exports = router;
