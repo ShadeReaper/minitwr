533396c1b0d003dbab247f7aa9896b2271786f96
Yanis <yanis.chibout@etu.unistra.fr>
2015-05-13 15:56:16 +0200

    Création de la possibilité d'ajouter un pseudo au tweet posté + trolls dans le formulaire

--- routes/accueil.js
+++ routes/accueil.js
@@ -3,14 +3,19 @@ var router = express.Router();
 
 var tweets = [];
 
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('accueil', { title: 'Watcher', tweets: tweets });
+  res.render('accueil', { title: 'Watcher', tweets: tweets});
 });
 
 router.post('/', function(req, res, next) {
-    tweets.unshift(req.body.tweet);
-    res.redirect('/accueil')
+    tweets.unshift({
+		tpost: req.body.tweet,
+		pseudo: req.body.pseudo
+	});
+    res.redirect('/accueil');
 });
 
 module.exports = router;
